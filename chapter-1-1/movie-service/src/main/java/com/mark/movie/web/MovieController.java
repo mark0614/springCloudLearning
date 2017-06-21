9af8e2f279f055ef37eec8613ac09f0f63f5ad56
mark0614@126.com <mark0614@126.com>
2017-06-21 09:55:27 +0800

    修改eureka-server的application.yml
    movie-service新增一个接口服务

--- chapter-1-1/movie-service/src/main/java/com/mark/movie/web/MovieController.java
+++ chapter-1-1/movie-service/src/main/java/com/mark/movie/web/MovieController.java
@@ -1,11 +1,13 @@
 package com.mark.movie.web;
 
+import com.mark.movie.service.MovieService;
 import com.mark.movie.dto.MovieDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,28 +17,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/movie")
 public class MovieController {
-
+    @Autowired
+    private MovieService movieService;
     @GetMapping(value = "list")
     public List<MovieDTO> list() {
-        List<MovieDTO> movieDTOList = new ArrayList<>();
-        MovieDTO movieDTO1 = MovieDTO.builder()
-                .id(1)
-                .title("异形 契约")
-                .director("雷德利·斯科特")
-                .screenTime("2017-06-16").build();
-        MovieDTO movieDTO2 = MovieDTO.builder()
-                .id(2)
-                .title("变形金刚5：最后的骑士")
-                .director(" 迈克尔·贝")
-                .screenTime("2017-06-23").build();
-        MovieDTO movieDTO3 = MovieDTO.builder()
-                .id(3)
-                .title("神偷奶爸3")
-                .director("凯尔·巴尔达")
-                .screenTime("2017-07-07").build();
-        movieDTOList.add(movieDTO1);
-        movieDTOList.add(movieDTO2);
-        movieDTOList.add(movieDTO3);
-        return movieDTOList;
+        return movieService.findMovies();
+    }
+
+    @GetMapping("{id}")
+    MovieDTO get(@PathVariable String id){
+        return movieService.findOne(id);
     }
 }
