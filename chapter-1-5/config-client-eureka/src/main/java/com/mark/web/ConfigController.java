f1c2cff391c828e46c9f84ae4fdfd08d155c8279
mark0614@126.com <mark0614@126.com>
2017-06-21 19:24:27 +0800

    Spring Cloud构建微服务架构----配置服务中心

---
+++ chapter-1-5/config-client-eureka/src/main/java/com/mark/web/ConfigController.java
@@ -0,0 +1,27 @@
+package com.mark.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * ConfigController:
+ * <p>
+ */
+@RestController
+@RequestMapping("/movie")
+public class ConfigController {
+    @Value("${upload.path}")
+    private String moviePath;
+
+    @GetMapping("path")
+    Map<String, Object> getPath() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("path", moviePath);
+        return map;
+    }
+}
